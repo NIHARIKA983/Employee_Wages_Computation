cb58fa72d895fe874a7fc95ac5b273fc2d6b0c31
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-05-19 20:33:18 +0530

    Added:To Add Part Time Employee And Wage

--- src/main/java/com/bridgelabz/EmployeWage.java
+++ src/main/java/com/bridgelabz/EmployeWage.java
@@ -5,12 +5,16 @@ public class EmployeWage {
         final int IS_PRESENT = 1;
         final int PER_HR = 20;
         final int FULL_DAY = 8;
+        final int PART_TIME_HRS=8;
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == IS_PRESENT) {
             System.out.println("employee is present");
             int oneday_wages = (PER_HR * FULL_DAY);
-            System.out.println(oneday_wages);
-
+            System.out.println("Oneday Wages is:"+oneday_wages);
+         int part=(PER_HR*PART_TIME_HRS);
+         double total = (oneday_wages+part);
+         System.out.println("PartTime Wages"+part);
+         System.out.println("TotalWages"+total);
         } else {
             System.out.println("employee is absent");
         }
