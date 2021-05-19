527193469a2c718ced7adc736e3adb2508061e60
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-05-19 23:22:54 +0530

    Added:To Calculate Wages For A Month

--- src/main/java/com/bridgelabz/EmployeWage.java
+++ src/main/java/com/bridgelabz/EmployeWage.java
@@ -1,34 +1,30 @@
 package com.bridgelabz;
 
 public class EmployeWage {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         final int IS_PRESENT= 1 ;
-        int PER_HR = 20;
-        int FULL_DAY = 8;
-        int PART_TIME_HRs = 8;
+        int PER_HR=20;
+        int FULL_DAY=8;
+        int PART_TIME_HRs=8;
         //int oneday_wages=0;
-        int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
-        if(empCheck==IS_PRESENT) {
-            System.out.println("Employee is Present");
-
-            switch (empCheck) {
-
-                case 0:
-                    int oneday_wages = (PER_HR * FULL_DAY);
-                    System.out.println("Oneday Wages is : " + oneday_wages);
-                    break;
-                case 1:
-                    int part = (PER_HR * PART_TIME_HRs);
-                    int Day = (PER_HR * FULL_DAY);
-                    int total = (part + Day);
-                    System.out.println("PartTime Wages " + part);
-                    System.out.println("TotalWages " + total);
-                    break;
-                default:
-                    System.out.println("Employee is Absent");
-
-
-            }
+        int empCheck= (int) (Math.floor(Math.random()*10)%2);
+        if(empCheck==IS_PRESENT)
+        {
+            int part= (PER_HR*PART_TIME_HRs);
+            int Day= (PER_HR * FULL_DAY );
+            int total=(part+Day);
+            int Gross_Total=(total*20);
+            System.out.println("PartTime Wages " + part);
+            System.out.println("TotalWages "+total);
+            System.out.println(Gross_Total);
         }
+        else{
+            int oneday_wages=(   PER_HR * FULL_DAY   );
+            System.out.println("Oneday Wages is : " + oneday_wages);
+        }
+
+
     }
+
 }
