a047fc1eddcc5b65550320f15770f779b2ff2aed
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-05-19 23:04:30 +0530

    Added:To Solve Using Switch Case Statement

--- src/main/java/com/bridgelabz/EmployeWage.java
+++ src/main/java/com/bridgelabz/EmployeWage.java
@@ -2,22 +2,33 @@ package com.bridgelabz;
 
 public class EmployeWage {
     public static void main(String[] args) {
-        final int IS_PRESENT = 1;
-        final int PER_HR = 20;
-        final int FULL_DAY = 8;
-        final int PART_TIME_HRS=8;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_PRESENT) {
-            System.out.println("employee is present");
-            int oneday_wages = (PER_HR * FULL_DAY);
-            System.out.println("Oneday Wages is:"+oneday_wages);
-         int part=(PER_HR*PART_TIME_HRS);
-         double total = (oneday_wages+part);
-         System.out.println("PartTime Wages"+part);
-         System.out.println("TotalWages"+total);
-        } else {
-            System.out.println("employee is absent");
-        }
+        final int IS_PRESENT= 1 ;
+        int PER_HR = 20;
+        int FULL_DAY = 8;
+        int PART_TIME_HRs = 8;
+        //int oneday_wages=0;
+        int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+        if(empCheck==IS_PRESENT) {
+            System.out.println("Employee is Present");
 
+            switch (empCheck) {
+
+                case 0:
+                    int oneday_wages = (PER_HR * FULL_DAY);
+                    System.out.println("Oneday Wages is : " + oneday_wages);
+                    break;
+                case 1:
+                    int part = (PER_HR * PART_TIME_HRs);
+                    int Day = (PER_HR * FULL_DAY);
+                    int total = (part + Day);
+                    System.out.println("PartTime Wages " + part);
+                    System.out.println("TotalWages " + total);
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+
+
+            }
+        }
     }
 }
