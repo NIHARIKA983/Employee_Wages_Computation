6973cebeb595c58acabfa0a457e7df54619f8a19
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-05-19 16:15:19 +0530

    Added:To Calcutate Daily Employee Wages

--- src/main/java/com/bridgelabz/EmployeWage.java
+++ src/main/java/com/bridgelabz/EmployeWage.java
@@ -1,15 +1,17 @@
 package com.bridgelabz;
 
 public class EmployeWage {
-    public static void main(String[] args){
-        final int IS_PRESENT= 1 ;
-        double empCheck= Math.floor(Math.random()*10)%2;
-        if (empCheck==IS_PRESENT)
-        {
+    public static void main(String[] args) {
+        final int IS_PRESENT = 1;
+        final int PER_HR = 20;
+        final int FULL_DAY = 8;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_PRESENT) {
             System.out.println("employee is present");
+            int oneday_wages = (PER_HR * FULL_DAY);
+            System.out.println(oneday_wages);
 
-        }
-        else {
+        } else {
             System.out.println("employee is absent");
         }
 
